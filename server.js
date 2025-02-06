7fcbc0e299fb227e6df1d08782027a80a0c4a0e9
Raghavan Venkataraman <guruprasadhraghavan@gmail.com>
2025-02-06 16:51:19 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
+    const express = require("express");
+    const app = express();
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
+    app.get("/ping", (req, res) => {
+    res.send("pong");
+    });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+    const PORT = 3000;
+    app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+    });
