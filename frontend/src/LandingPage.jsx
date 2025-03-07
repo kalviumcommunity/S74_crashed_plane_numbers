92921ad2d7ed34e6fb9bb35e0b710a4b7d45e6b3
Guruprasadh-R <guruprasadhraghavan@gmail.com>
2025-03-07 19:48:37 +0530

    Update LandingPage.jsx

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,12 +1,12 @@
 
-import PlaneCrashList from "./components/PlaneCrashList.jsx";
+
 
 function LandingPage() {
     return (
         <div style={{ textAlign: "center", padding: "50px" }}>
             <h1>Welcome to the ASAP Project</h1>
             <p>This is the landing page for our ASAP idea.</p>
-            <PlaneCrashList />
+            
         </div>
     );
 }
